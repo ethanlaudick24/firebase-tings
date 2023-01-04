1b5c208afc00a44bc05e7170b1b3f33b4b8459ec
Ethan <ethan.laudick24@d155.org>
2023-01-04 07:41:27 -0600

    Initial Commit

---
+++ firebase tings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase tings
+//
+//  Created by ETHAN LAUDICK on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
